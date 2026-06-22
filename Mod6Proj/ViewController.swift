32708234a8879a3a36c5a51c5f20428b7d14f863
jbaldwin12 <jtb0185@auburn.edu>
2026-06-22 11:38:03 -0500

    Initial Commit

---
+++ Mod6Proj/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mod6Proj
+//
+//  Created by Charlie Hudnall on 6/22/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
